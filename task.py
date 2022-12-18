eec20c2b8a23db4b524b4b8debc1b2f773c3b9c7
baxronovferuz1 <baxronovvferuz25@gmail.com>
2022-12-18 13:15:08 +0500

    add matter

--- task.py
+++ task.py
@@ -82,3 +82,36 @@ for i in range(N):
     countries.add(input())
 
 print(len(countries))
+
+
+#Integers Come In All Sizes
+
+
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+print(a**b+c**d)
+
+
+
+
+#Capitalize
+
+
+
+def solve(s):
+    for i in s.split():
+        s=s.replace(i,i.capitalize())
+    return s
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
