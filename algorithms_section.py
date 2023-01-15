d03ad9e5b7c14d1c8068a1068dcfcef1453de4c2
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-01-15 20:36:12 +0500

    commmit

--- algorithms_section.py
+++ algorithms_section.py
@@ -71,3 +71,36 @@ while n>n-n:
     print((n-1)*m+t*("#"))
     n=n-1
     t=t+1
+
+
+
+#A Very Big Sum
+
+import math
+import os
+import random
+import re
+import sys
+
+def aVeryBigSum(ar):
+    return sum(ar)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
+
+#Birthday Cake Candles
+
+N=int(input())
+candles=list(map(int,input().split()))
+print(candles.count(max(candles)))
