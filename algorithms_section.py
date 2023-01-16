a71428de07494206c616ea83c59f180070587184
baxronovferuz1 <baxronovvferuz25@gmail.com>
2023-01-16 17:41:04 +0500

    commit

--- algorithms_section.py
+++ algorithms_section.py
@@ -104,3 +104,61 @@ if __name__ == '__main__':
 N=int(input())
 candles=list(map(int,input().split()))
 print(candles.count(max(candles)))
+
+
+
+#Beautiful Days at the Movies
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+def beautifulDays(i, j, k):
+    count=int(0)
+    for i in range(i,j+1):
+        if (i-int(str(i)[::-1]))%k==0:
+            count+=1
+    return count
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    i = int(first_multiple_input[0])
+
+    j = int(first_multiple_input[1])
+
+    k = int(first_multiple_input[2])
+
+    result = beautifulDays(i, j, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
+
+
+
+#Cats and a Mouse
+
+
+import sys
+
+
+q = int(input().strip())
+for i in range(q):
+    x,y,z = input().strip().split(' ')
+    x,y,z = [int(x),int(y),int(z)]
+    
+    if abs(x - z) < abs(y - z):
+        print('Cat A')
+    elif abs(x - z) > abs(y - z):
+        print('Cat B')
+    else:
+        print('Mouse C')
